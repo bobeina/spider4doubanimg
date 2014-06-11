f38b1fb671a3e3fc23fad575f11f82a7967bdfca
aliao <minvacai@sina.com>
2014-06-11 19:17:24 +0800

    parse(): crawl specific group(s) with imgurls stable

--- sp1/pipelines.py
+++ sp1/pipelines.py
@@ -8,33 +8,19 @@ import json
 
 class Sp1Pipeline(object):
 	def __init__(self):
-		#self.file = codecs.open("/tmp/out.json", "wb", encoding="utf-8")
-		#filename = response.url.split("/")[-2]
-		#self.file = codecs.open(filename, "wb", encoding="utf-8")
 		pass
 	
 	def process_item(self, item, spider):
 		filename = item['filenm']
 		self.file = codecs.open(filename, "a", encoding="utf-8")
-		#line = json.dumps(dict(item))#, ensure_ascii=False)# + "\n"
-		line = u"[title]\n%s\n[filename]\n%s\n[link]\n%s\n[img urls]\n%s\n[content]\n%s" % ( item['title'], item['filenm'], item['link'], item['imgurls'], item['content'])
+		line = u"[title]\n%s\n[link]\n%s\n[img urls]\n%s\n[content]\n%s" % ( item['title'], item['link'], item['imgurls'], item['content'])
 		self.file.write(line)
 		self.file.close()
 		return item
 	
 	#def spider_closed(self, spider):
 	def close_spider(self, spider):
-		#self.file.close()
 		pass
 
 
-'''
-class Sp1Pipeline(object):
-    def __init__(self):
-	self.file = open('data.txt','wb')
-
-    def process_item(self, item, spider):
-	self.file.write(item['title'] + '\t' + item['link'] + '\t' + item['desc']+'\n')
-        #return item
-'''
 
--- sp1/spiders/sp1_spider.py
+++ sp1/spiders/sp1_spider.py
@@ -12,24 +12,12 @@ import re
 class Sp1Spider(Spider):#(BaseSpider):
 	name = "douban.com" #"fx168.com"
 	start_urls = [
-			#"http://www.fx168.com/forex/cad/1405/1013688.shtml"
-			#"http://news.fx168.com/forex/",
-			#"http://news.fx168.com/bank/"
-			#"http://www.douban.com/group/explore"
-			#"http://www.douban.com/"
 			#"http://www.douban.com/group/haixiuzu",
 			"http://www.douban.com/group/meituikong"
 			]
 	rootdir = '/tmp/spiderData/'
-
         allowed_domains = ['douban.com']
 
-	'''
-        rules = (
-            #Rule(SgmlLinkExtractor(allow=(r'http://www.qunar.com/routes/.*')), callback='parse'),
-            #Rule(SgmlLinkExtractor(allow=('http:.*/routes/.*')), callback='parse'),
-        )
-	'''
 
         def parse(self, response):
 		#item = Sp1Item()
@@ -40,14 +28,12 @@ class Sp1Spider(Spider):#(BaseSpider):
 		pattern_topic = re.compile(r'http://www.douban.com/group/topic/(\d+)/')
 		pattern_topic_list = re.compile(r'http://www.douban.com/group/(\w+)/discussion\?start=(\d+)')
 		for url in raw_urls:
-			#print '=================> url = ',url
 			match_list = pattern_topic_list.match(url)
 			match_topic = pattern_topic.match(url)
 			if match_list or match_topic:
 				urls.append(url)
 		
 		for url in urls:
-			#yield Request(url)
 			yield Request(url,meta = {'item':url},callback=self.parse)
 			
 		# get img
@@ -62,7 +48,6 @@ class Sp1Spider(Spider):#(BaseSpider):
 				imgurls = hxs.xpath("//div[@class='topic-figure cc']/img/@src").extract()
 				imgurls_txt = ''
 				
-				#print '>>>>>>>>>>>>>>> img urls = ',imgurls
 				imgurls_txt = ';'.join(imgurls)
 				crnt_item = Sp1Item()
 				crnt_item['title'] = topic[0]
@@ -70,68 +55,5 @@ class Sp1Spider(Spider):#(BaseSpider):
 				crnt_item['link'] = response.url
 				crnt_item['content'] = desc
 				crnt_item['imgurls'] = imgurls_txt
-				#item.append(crnt_item)
 				yield crnt_item
-
-        '''
-	def parse2(self, response):
-		hxs = HtmlXPathSelector(response)
-		item = response.meta['item']
-		items = []
-		contents = hxs.select('//p')
-
-		for content in contents:
-			desc = content.xpath('text()').extract()
-			if True:#desc:
-				crnt_item = Sp1Item() #contentItem()
-				crnt_item['content'] = desc
-				crnt_item['filenm'] = self.rootdir + response.url.split("/")[-2]+"_"+response.url.split("/")[-1]
-				items.append(crnt_item)
-
-		return items
-
-
-	def parse(self, response):
-		hxs = Selector(response)
-		items = []
-
-		sites = hxs.xpath('//a') #hxs.xpath('//a/text()').extract()
-		base_url = get_base_url(response)
-
-		validurls = []
-
-		topic_url = 'topic'
-		i = 0
-		for site in sites:
-			if i>2:
-				break
-
-			url = site.xpath('@href').extract()
-			#print '        <<<',url,'>>>'
-			if url[0]:
-				if url[0][0]=='#':
-					continue
-				if url[0][0]=='/' or url[0][0]=='?' :
-					relative_url = urljoin_rfc(base_url,url[0])
-					#print '----------------------',relative_url[0]
-				else:
-					relative_url = url[0]
-			else:
-				continue
-
-			if topic_url not in relative_url:
-				#print u'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%s'%(relative_url)
-				continue
-			
-			item = Sp1Item()
-			item['link'] = relative_url
-			item['title'] =site.xpath('text()').extract()
-			validurls.append(item)
-			i = i+1
-
-		for crntlink in validurls:
-			yield Request(crntlink['link'],meta = {'item':crntlink},callback=self.parse)
-			yield Request(crntlink['link'],meta = {'item':crntlink},callback=self.parse2)
-	'''
-
 
